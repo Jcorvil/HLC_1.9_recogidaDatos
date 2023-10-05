136b391b6989c37d33c32e1a7437e5d604060f2c
unknown <jcorvil600@g.educaand.es>
2023-10-05 13:22:53 +0200

    .

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,7 +6,48 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  altura: number = 0;
+  peso: number = 0;
+  imc: number = 0;
+  txtMensaje: string = "";
+  txtIMC: string = "" ;
 
-  constructor() {}
+  constructor() { }
+
+  calculoIMC() {
+    this.imc = this.peso / (this.altura * this.altura);
+    this.clasificacionIMC();
+  }
+
+  clasificacionIMC() {
+    if (this.imc < 18.5) {
+      console.log("Se encuentra con un peso inferior recomendado para su altura");
+      this.txtMensaje = "Se encuentra con un peso inferior recomendado para su altura";
+    } else if (this.imc > 18.5 && this.imc < 24.9) {
+      console.log("Se encuentra dentro del rango saludable para su altura");
+      this.txtMensaje = "Se encuentra dentro del rango saludable para su altura";
+    } else if (this.imc > 25 && this.imc < 29.9 ) {
+      console.log("Tiene un ligero exceso de peso con respecto a lo recomendado para su altura, aunque dentro de lo saludable");
+      this.txtMensaje = "Tiene un ligero exceso de peso con respecto a lo recomendado para su altura, aunque dentro de lo saludable";
+    } else if (this.imc > 30 && this.imc <34.9){
+      console.log("Se encuentra dentro del rango de obesidad leve");
+      this.txtMensaje = "Se encuentra dentro del rango de obesidad leve";
+    } else if (this.imc > 35 && this.imc <39.9){
+      console.log("Se encuentra dentro del rango de obesidad moderada");
+      this.txtMensaje = "Se encuentra dentro del rango de obesidad moderada";
+    } else if (this.imc >= 40) {
+      console.log("Se encuentra dentro del rango de obesidad mórbida");
+      this.txtMensaje = "Se encuentra dentro del rango de obesidad mórbida";
+    }
+  }
+
+  reiniciar() {
+    this.clasificacionIMC();
+    this.altura = 0;
+    this.peso = 0;
+    this.imc = 0;
+    this.txtMensaje = "";
+    this.txtIMC = "" ;
+  }
 
 }
